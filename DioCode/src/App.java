6cb114b6fdc322453cdb245b3167e5ff8f4922f0
Gustavopsilva84 <gustavopsilva84@hotmail.com>
2024-11-05 19:55:54 -0300

    Add files via upload

---
+++ DioCode/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numero = 0;
+        
+        System.out.println("QuAL O numero?"+numero);
+        System.out.println("Olá Tudo bem?");
+
+    }
+}
